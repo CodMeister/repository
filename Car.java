64866bf73bdfd86de968e37654b85e500117c9cb
CodMeister <115082613+CodMeister@users.noreply.github.com>
2022-10-25 22:10:30 +0900

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,26 @@
+package ch07;
+//재정의할 수 없는 final 메소드
+public class Car {
+	//필드
+	//자동차는 4개의 타이어를 가진다.
+	Tire frontLeftTire = new Tire("앞왼쪽", 6);
+	Tire frontRightTire = new Tire("앞오른쪽", 2);
+	Tire backLeftTire = new Tire("뒤왼쪽", 3);
+	Tire backRightTire = new Tire("뒤오른쪽", 4);
+	
+	//생성자
+	
+	//메소드
+	int run() {
+		System.out.println("[자동차가 달립니다.]");
+		if(frontLeftTire.roll()==false) {stop(); return 1;}
+		if(frontRightTire.roll()==false) {stop(); return 2;}
+		if(backLeftTire.roll()==false) {stop(); return 3;}
+		if(backRightTire.roll()==false) {stop(); return 4;}
+		return 0;
+	}
+
+	void stop() { //Tire의 roll() 값이 false가 나왔을 때 실행
+		System.out.println("[자동차가 멈춥니다.]");
+	}
+}
