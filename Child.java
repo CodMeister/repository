0019160e998a0cd30af08fe67229ca56cec3b657
CodMeister <115082613+CodMeister@users.noreply.github.com>
2022-10-26 21:59:14 +0900

    Add files via upload

--- Child.java
+++ Child.java
@@ -1,10 +1,5 @@
 package ch07;
 
 public class Child extends Parent{
-	//필드
-	public String field2;
-	//메소드
-	public void method3() {
-		System.out.println("Child-method3()");
-	}
+	
 }
--- Parent.java
+++ Parent.java
@@ -1,15 +1,5 @@
 package ch07;
 //부모클래스
 public class Parent {
-	//필드
-	public String field1;
 	
-	//메소드
-	public void method1() {
-		System.out.println("Parent-method1()");
-	}
-	
-	public void method2() {
-		System.out.println("Parent-method2()");
-	}
 }
